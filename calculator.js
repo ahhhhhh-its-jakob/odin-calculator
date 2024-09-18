a2da896cf32d0d9394c575c1a7a5c591d5bb3eca
Jakob <codemonkey727@gmail.com>
2024-09-18 18:02:32 -0400

    create functions for basic math operations

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(numerator, denominator){
+    return numerator / denominator;
+}
+
+
