a55010245cd60ac3e88e7307e6e621dc32ec4116
Jakob <codemonkey727@gmail.com>
2024-09-18 19:05:13 -0400

    add functionality to have a working display when pressing number buttons

--- calculator.js
+++ calculator.js
@@ -1,7 +1,12 @@
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = '';
+let displayValue = 0;
 
+const display = document.querySelector('.display');
+const calculator = document.querySelector('.button-container');
+
+calculator.addEventListener("click", handleClick);
 
 function add(num1, num2){
     return num1 + num2;
@@ -19,6 +24,41 @@ function divide(numerator, denominator){
     return numerator / denominator;
 }
 
-function operate(num1, num2, operator){
+function handleClick(event){
+    let input = event.target.innerText;
 
+    if(!isNaN(input)){
+        firstNumber += input;
+        changeDisplay(input)
+    }
+}
+
+
+function operate(num1, num2, operator){
+    switch(operator){
+        case '+': 
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
+
+function changeDisplay(value){
+    display.innerText += value;
+}
+
+function clear(){
+    display.innerText = 0;
+    firstNumber = 0;
+    secondNumber = 0;
 }
