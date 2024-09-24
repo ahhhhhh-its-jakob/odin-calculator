3dfbd99600eb4460062f2da91152099f88dc490a
Jakob <codemonkey727@gmail.com>
2024-09-24 18:05:45 -0400

    rework how numbers display and numbers are assigned

--- calculator.js
+++ calculator.js
@@ -1,64 +1,96 @@
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber = '';
+let secondNumber = '';
 let operator = '';
-let displayValue = 0;
+let runningTotal = 0;
+const operators = ['+','-','*','/','='];
 
 const display = document.querySelector('.display');
 const calculator = document.querySelector('.button-container');
-
 calculator.addEventListener("click", handleClick);
 
 function add(num1, num2){
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2){
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2){
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(numerator, denominator){
-    return numerator / denominator;
+    return Number(numerator) / Number(denominator);
 }
 
 function handleClick(event){
     let input = event.target.innerText;
+    let isOperator = operators.includes(input);
 
-    if(!isNaN(input)){
+    if(!isOperator && operator == ''){
         firstNumber += input;
-        changeDisplay(input)
+        changeDisplay(firstNumber);
+    }
+
+    if(!isOperator && operator != ''){
+        secondNumber += input;
+        changeDisplay(secondNumber);
+    }
+
+    if(isOperator){
+        if(operator == ''){
+            operator = input;
+            changeDisplay(secondNumber);
+        }
+
+        if(input == '='){
+            console.log(firstNumber, secondNumber, operator)
+        }
+    }
+
+    if(input == 'Clear'){
+        clear();
     }
 }
 
+function changeDisplay(value){
+    let isNumber = !isNaN(value);
+
+    if(isNumber){
+        display.innerText = value;
+    }
+
+    if(value === ''){
+        display.innerText = 0;
+    }
+}
 
 function operate(num1, num2, operator){
+    console.log(num1, num2, operator);
+    let total;
+
     switch(operator){
         case '+': 
-            add(num1, num2);
+            total = add(num1, num2);
             break;
         case '-':
-            subtract(num1, num2);
+            total = subtract(num1, num2);
             break;
         case '*':
-            multiply(num1, num2);
+            total = multiply(num1, num2);
             break;
         case '/':
-            divide(num1, num2);
-            break;
+            total = divide(num1, num2);
         default:
             break;
     }
 }
 
-function changeDisplay(value){
-    display.innerText += value;
-}
-
 function clear(){
-    display.innerText = 0;
+    changeDisplay('clear');
     firstNumber = 0;
     secondNumber = 0;
+    operator = '';
+    runningTotal = 0;
 }
